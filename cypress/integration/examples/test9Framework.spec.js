5dad5fc61dee541da452b7c99f57a7eb3da37883
Ariuna <ariun0188@gmail.com>
2022-08-08 19:58:03 -0700

    updated test 9

--- cypress/integration/examples/test9Framework.spec.js
+++ cypress/integration/examples/test9Framework.spec.js
@@ -1,13 +1,14 @@
 /// <reference types="Cypress" />
 import HomePage from "../pageObjects/HomePage"
 import ProductPage from "../pageObjects/productPage";
+
 const homePage = new HomePage()
 const productPage = new ProductPage()
 describe('Framework test', function () {
   before('', () => {
     cy.visit('https://rahulshettyacademy.com/angularpractice/')
-    cy.fixture('example').then(function(data){
-      this.data=data
+    cy.fixture('example').then(function (data) {
+      this.data = data
     })
   })
   it('demo test', function () {
@@ -19,16 +20,31 @@ describe('Framework test', function () {
     Cypress.config('defaultCommandTimeout', 8000) //set up only in one test case
     homePage.getShopTab().click()
 
-    this.data.productName.forEach(function (element){
-    cy.selectProduct(element)
+    this.data.productName.forEach(function (element) {
+      cy.selectProduct(element)
     })
     productPage.checkoutButton().click()
+    let sum = 0
+    cy.get('tr td:nth-child(4) strong').each((el, index, list) => {
+      const amount = el.text()
+      let res = amount.split(' ')
+      res = res[1].trim()
+      sum = Number(sum) + Number(res)
+    }).then(function () {
+      cy.log(sum)
+    })
+    cy.get('h3 strong').then(function (element) {
+      const amount = element.text()
+      let res = amount.split(' ')
+      let total = res[1].trim()
+      expect(Number(total)).to.equal(sum)
+        })
     cy.contains('Checkout').click()
     cy.get('#country').type('India')
     cy.get('.suggestions > ul > li > a').click()
     cy.get('#checkbox2').click({force: true}) //covered by element
     cy.get('input[type="submit"]').click()
-    cy.get('.alert').then(function (element){ // resolving promise, catching obj in element
+    cy.get('.alert').then(function (element) { // resolving promise, catching obj in element
       const actualText = element.text()
       expect(actualText.includes('Success')).to.be.true
     })
