8bf6e51bdcb702a30f2880cace6bb8bbbfabac67
Ariuna <ariun0188@gmail.com>
2022-08-09 22:08:20 -0700

    jenkins build 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "9b2puc",
+  projectId: "kgzihd",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
